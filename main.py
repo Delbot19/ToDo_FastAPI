03d6f78a62be7f42910488f7e496f7798d61c394
Delbot19 <delbotoussou00@gmail.com>
2023-05-17 16:49:38 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
+"""
+Il s'agit de la page main.
+"""
 from fastapi import FastAPI
 
 app=FastAPI()
 
 @app.get("/")
 async def index():
+    """
+        Il s'agit de la page main.
+    """
     return {"message":"Hello world"}
-
-
-@app.post("/")
-async def post():
-    return {"message":"Hello world from post route"}
-
-@app.put("/")
-async def put():
-    return {"message":"Hello world from put route"}   
-
-
-@app.get("/items")
-async def listItems():
-    return {"message":"list items route"}
-
-
-@app.get("/items/{item_id}")
-async def get_id(item_id:int):
-    return {"item_id":item_id}
